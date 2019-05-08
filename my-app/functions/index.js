aec7e0a4b5d578b928af20638dd934076bbdf168
RabbitQAQ <charles_dai@qq.com>
2019-05-08 12:58:41 -0500

    firebase function init

---
+++ my-app/functions/index.js
@@ -0,0 +1,21 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+function analyseJson(json) {
+
+}
+
+function sendEmail(json) {
+
+}
+
+exports.scheduledFunctionCrontab =
+    functions.pubsub.schedule('56 12 * * *').timeZone('America/Chicago').onRun((context) => {
+        // analyseJson();
+        // sendEmail();
+        return admin.database().ref('/messages').push({original: "original333333333333"});
+    });
