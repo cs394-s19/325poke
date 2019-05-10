9fb0e4d1c044fce8b63f67366601ce2493e9b8b3
Keren Park <parkhk10@gmail.com>
2019-05-10 10:51:47 -0500

    write function to get slacker ids

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function fetchJson() {
+  // fetch data from local file
+  // TODO: In the future, the data would be fetched from Prof.Riesbeck's server
+  let json = require('./poke-325-export.json');
+  return json;
+}
+
+function getAnyReminders() {
+  // return an array with author ids
+  const submissionData = fetchJson()
+  const slackers = []
+  const date = new Date();
+  var currentTime = date.getTime()
+  for (var authorId in submissionData["authors"]) {
+    var lastSubTime = mostRecentSubTime(authorId, currentTime)
+    if (lastSubTime - currentTime > TimeUnit.DAYS.toMillis(4)) {
+      slackers.concat([authorId])
+    }
+  }
+  console.log("slackers: " + slackers)
+  return slackers;
+}
+
+getAnyReminders()
+
 function App() {
   return (
     <div className="App">
