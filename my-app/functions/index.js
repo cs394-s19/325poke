b0c8ba77b38c3a16da9508fe0b5cb25d7bf66b86
RabbitQAQ <charles_dai@qq.com>
2019-05-09 20:06:13 -0500

    update and reformat json data

--- my-app/functions/index.js
+++ my-app/functions/index.js
@@ -5,21 +5,49 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-admin.database().ref('/authors').once('value').then(function(snapshot) {
-    console.log(snapshot.val());
-});
+function fetchJson() {
+    // fetch data from local file
+    // TODO: In the future, the data would be fetched from Prof.Riesbeck's server
+    let json = require('./example-submission-data.json');
+    return json;
+}
 
-function analyseJson(json) {
+function updateJson(originJson, json) {
+    let formattedJson = originJson === null ? {} : originJson;
+    // fetch formatted json from database
+    if (!formattedJson.hasOwnProperty('authors')) {
+        formattedJson['authors'] = {};
+    }
+    // reformat the json and also compare & merge with database
+    let keyList = Object.keys(json['submissions']);
+    keyList.forEach((key) => {
+        let submitObject = json['submissions'][key];
+        if (!formattedJson['authors'].hasOwnProperty(submitObject['author'])) {
+            formattedJson['authors'][submitObject['author']] = {};
+            formattedJson['authors'][submitObject['author']]['submissions'] = {}
+        }
+        if (!formattedJson['authors'][submitObject['author']]['submissions'].hasOwnProperty(submitObject['exid'])) {
+            formattedJson['authors'][submitObject['author']]['submissions'][submitObject['exid']] = {};
+            formattedJson['authors'][submitObject['author']]['submissions'][submitObject['exid']]["submitted"] = submitObject['submitted'];
+        } else if (formattedJson['authors'][submitObject['author']]['submissions'][submitObject['exid']]["submitted"] < submitObject['submitted'])
+            formattedJson['authors'][submitObject['author']]['submissions'][submitObject['exid']]["submitted"] = submitObject['submitted'];
+        formattedJson['authors'][submitObject['author']]['submissions'][submitObject['exid']]["status"] = submitObject['status'];
+    });
 
+    return formattedJson;
 }
 
 function sendEmail(json) {
 
 }
 
-exports.scheduledFunctionCrontab =
-    functions.pubsub.schedule('56 12 * * *').timeZone('America/Chicago').onRun((context) => {
-        // analyseJson();
-        // sendEmail();
-        return admin.database().ref('/messages').push({original: "original333333333333"});
+exports.updateDatabaseAndSendEmail =
+    functions.pubsub.schedule('0 20 * * *').timeZone('America/Chicago').onRun((context) => {
+        // query database
+        return admin.database().ref('/').once('value').then((snapshot) => {
+            // when query finished, call updatejson() to compare and "merge" the current data in database with new json data
+            let newJson = updateJson(snapshot.val(), fetchJson());
+            // upload the data to database
+            admin.database().ref('/').update(newJson);
+        });
     });
--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -11,6 +11,50 @@ firebase.initializeApp(config);
 // Get a reference to the database service
 let database = firebase.database();
 
-database.ref('/authors').once('value').then(function(snapshot) {
-    console.log(snapshot.val());
-});
+function fetchJson() {
+    // Fetch data from local file
+    // TODO: In the future, the data would be fetched from Prof.Riesbeck's server
+    let json = require('./resources/example-submission-data.json');
+    return json;
+}
+
+// fetch json and use it to update the database
+function updateSubmissionInDatabase() {
+    // query database
+    let ref = database.ref('/').once('value').then((snapshot) => {
+        // when query finished, call updatejson() to compare and "merge" the current data in database with new json data
+        let newJson = updateJson(snapshot.val(), fetchJson());
+        console.log(newJson);
+        // upload the data to database
+        database.ref('/').update(newJson);
+    });
+}
+
+function updateJson(originjson, json) {
+    let formattedJson = originjson;
+    // fetch formatted json from database
+    if (!formattedJson.hasOwnProperty('authors')) {
+        formattedJson['authors'] = {};
+    }
+    // reformat the json and also compare & merge with database
+    let keyList = Object.keys(json['submissions']);
+    keyList.forEach((key) => {
+        let submitObject = json['submissions'][key];
+        if (!formattedJson['authors'].hasOwnProperty(submitObject['author'])) {
+            formattedJson['authors'][submitObject['author']] = {};
+            formattedJson['authors'][submitObject['author']]['submissions'] = {}
+        }
+        if (!formattedJson['authors'][submitObject['author']]['submissions'].hasOwnProperty(submitObject['exid'])) {
+            formattedJson['authors'][submitObject['author']]['submissions'][submitObject['exid']] = {};
+            formattedJson['authors'][submitObject['author']]['submissions'][submitObject['exid']]["submitted"] = submitObject['submitted'];
+        } else if (formattedJson['authors'][submitObject['author']]['submissions'][submitObject['exid']]["submitted"] < submitObject['submitted'])
+            formattedJson['authors'][submitObject['author']]['submissions'][submitObject['exid']]["submitted"] = submitObject['submitted'];
+        formattedJson['authors'][submitObject['author']]['submissions'][submitObject['exid']]["status"] = submitObject['status'];
+    });
+
+    return formattedJson;
+}
+
+updateSubmissionInDatabase();
+
+// database.ref('/').update(newJson);
